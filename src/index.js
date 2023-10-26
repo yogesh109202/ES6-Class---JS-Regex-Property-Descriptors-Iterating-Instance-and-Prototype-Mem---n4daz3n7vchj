e1fba7627fd8333c7c2d9b8c08d9c760d052ae99
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2023-10-26 18:48:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 //Write your code here
 
+class API {
+    #secure
+    constructor(url, method = 'GET') {
+        this.url = url
+        this.method = method
+        this.#secure = url.startsWith("https")
+    }
 
+    isSecure() {
+        return this.#secure
+    }
+    updateUrl(url) {
+        this.url = url
+        this.#secure = url.startsWith("https")
+    }
+}
 
-
-
-//Do not remove this code
 module.exports = { API }
+
+
+
